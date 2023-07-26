ad495c48b334836c51ae0a8b5420fa5f2e1ed164
mehedi891 <mdmehedihasan891@gmail.com>
2023-07-26 16:09:30 +0600

    class 2 data types

---
+++ index.js
@@ -0,0 +1,32 @@
+/*
+
+Data types in JS
+
+1. String
+
+2. Number
+
+3.Object type
+
+4. Boolean 
+
+5. Undefined 
+
+6. Null
+
+
+*/
+
+let stringType = "this is string type data 57755 don't ";
+
+let numberType = 12;
+
+let BooleanType = true;
+
+let objectType = {'firstName':'abc','age':20, 'jobStatus': true};
+
+
+
+
+
+console.log(UndefinedType)
